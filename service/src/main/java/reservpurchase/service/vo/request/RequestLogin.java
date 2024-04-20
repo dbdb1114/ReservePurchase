74c3cb3fdb5022329d4ee5311dcf3e5c1a96c74e
yujeonghyeon <dbdb1114@naver.com>
2024-04-20 13:17:36 +0900

    로그인 요청 Vo와 응답 Member객체 생성 #4

--- service/src/main/java/reservpurchase/service/vo/request/RequestLogin.java
+++ service/src/main/java/reservpurchase/service/vo/request/RequestLogin.java
@@ -4,7 +4,6 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
-
 @Data
 public class RequestLogin {
     @NotNull(message = "Email cannnot be null")
