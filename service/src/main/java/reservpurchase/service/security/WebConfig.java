59d53c2062d78b7534d39e389262f89af48e80e1
yujeonghyeon <dbdb1114@naver.com>
2024-04-20 13:05:48 +0900

    Security를 활용한 로그인 로직 처리 및 토큰 발급 필터 생성 #3

--- service/src/main/java/reservpurchase/service/security/WebConfig.java
+++ service/src/main/java/reservpurchase/service/security/WebConfig.java
@@ -1,32 +1,64 @@
 package reservpurchase.service.security;
 
-import static org.springframework.security.config.Customizer.withDefaults;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import reservpurchase.service.service.MemberService;
 
 @Configuration
-@EnableWebSecurity
 public class WebConfig {
 
+    private PasswordEncoder passwordEncoder;
+    private MemberService memberService;
+    private Environment env;
+
+    @Autowired
+    public WebConfig(Environment env, MemberService memberService, PasswordEncoder passwordEncoder) {
+        this.env = env;
+        this.memberService = memberService;
+        this.passwordEncoder = passwordEncoder;
+    }
+
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
-        http.csrf(AbstractHttpConfigurer::disable)
-                .formLogin(httpSecurityFormLoginConfigurer -> httpSecurityFormLoginConfigurer.disable())
-                .httpBasic(httpSecurityHttpBasicConfigurer -> httpSecurityHttpBasicConfigurer.disable())
-                .headers(withDefaults())
-                .authorizeHttpRequests(custom->{
-                    custom.requestMatchers("/**").permitAll()
-                            .requestMatchers("/h2-console")
-                            .permitAll()
-                            .anyRequest()
-                            .permitAll();
-                });
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        AuthenticationManager authenticationManager = authenticationManager(http.getSharedObject(AuthenticationConfiguration.class));
+
+        http.csrf(config->config.disable());
+        http.authorizeHttpRequests(custom->custom.requestMatchers("/member-service/join").permitAll()
+                .requestMatchers("/member-service/temp-join").permitAll());
+
+        http.addFilter(getAuthenticationFilter(authenticationManager));
+        http.headers(custom->custom.disable());
+
         return http.build();
+    }
+
+    private AuthenticationFilter getAuthenticationFilter(AuthenticationManager authenticationManager) {
+        AuthenticationFilter authenticationFilter = new AuthenticationFilter(authenticationManager, memberService, env);
+        return authenticationFilter;
+    }
+
+    @Bean
+    AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
+            throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    //Override 해주는 용도
+    @Bean
+    AuthenticationProvider AuthenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(memberService);
+        provider.setPasswordEncoder(passwordEncoder);
+        return provider;
     }
 
 }
