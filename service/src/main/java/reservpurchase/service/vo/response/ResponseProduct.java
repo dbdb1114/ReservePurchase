5dc4dbd4d8fceb2d5ea05a63fe35158051c123f1
yujeonghyeon <dbdb1114@naver.com>
2024-04-22 05:49:15 +0900

    응답용 ResponseProduct 객체 구현 #9

---
+++ service/src/main/java/reservpurchase/service/vo/response/ResponseProduct.java
@@ -0,0 +1,20 @@
+package reservpurchase.service.vo.response;
+
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@ToString
+@EqualsAndHashCode
+@NoArgsConstructor
+public class ResponseProduct {
+
+    private Long id;
+    private String name;
+    private Integer price;
+    private Integer stock;
+
+}
