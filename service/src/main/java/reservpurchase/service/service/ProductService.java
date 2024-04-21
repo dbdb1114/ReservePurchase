b7053cf8a6f2b08369a844c03c693e437a61eabb
yujeonghyeon <dbdb1114@naver.com>
2024-04-22 05:47:01 +0900

    상품 상세 정보 가져오는 ProductDetail함수 생성 #9

---
+++ service/src/main/java/reservpurchase/service/service/ProductService.java
@@ -0,0 +1,10 @@
+package reservpurchase.service.service;
+
+import java.util.List;
+import reservpurchase.service.dto.ProductDto;
+
+public interface ProductService {
+
+    public ProductDto productDetail(Long id);
+
+}
