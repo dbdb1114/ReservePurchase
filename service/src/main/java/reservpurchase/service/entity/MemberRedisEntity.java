962dd230f6e03e6b69110259390a6d4b538fffb7
yujeonghyeon <dbdb1114@naver.com>
2024-04-21 18:34:29 +0900

    @Data 어노테이션 삭제 #4

--- service/src/main/java/reservpurchase/service/entity/MemberRedisEntity.java
+++ service/src/main/java/reservpurchase/service/entity/MemberRedisEntity.java
@@ -1,11 +1,9 @@
 package reservpurchase.service.entity;
 
-import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.redis.core.RedisHash;
 import reservpurchase.service.entity.embeded.Address;
 
-@Data
 @RedisHash(value = "member", timeToLive = 3600) // 한 시간 이내에 인증해야함.
 public class MemberRedisEntity {
 
@@ -17,5 +15,9 @@ public class MemberRedisEntity {
     private String certificationNumber;
     private Boolean emailAuthentication;
     private Address address;
+
+    public boolean certification(String certificationNumber){
+        return this.certificationNumber.equals(certificationNumber);
+    }
 
 }
