2f2031bfaf31947c2d308d4697383c1dbc721c94
yujeonghyeon <dbdb1114@naver.com>
2024-04-22 10:42:42 +0900

    paging 객체 생성 #8

---
+++ service/src/main/java/reservpurchase/service/dto/Paging.java
@@ -0,0 +1,19 @@
+package reservpurchase.service.dto;
+
+import lombok.Data;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
+
+@Data
+public class Paging {
+    private int page;
+    private int limit;
+    private String orderBy;
+    private String direction;
+
+    public PageRequest getPageRequest(){
+        return PageRequest.of(page, limit, Sort.by(Direction.valueOf(direction), orderBy));
+    }
+
+}
