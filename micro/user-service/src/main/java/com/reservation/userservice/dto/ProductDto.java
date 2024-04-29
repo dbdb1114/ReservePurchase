60714617e215794c53b2030437a0c81dc71ac2e8
yujeonghyeon <dbdb1114@naver.com>
2024-04-29 16:20:06 +0900

    상품 Dto 생성 #16

---
+++ micro/user-service/src/main/java/com/reservation/userservice/dto/ProductDto.java
@@ -0,0 +1,23 @@
+package com.reservation.userservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Getter
+@Builder
+@ToString
+@EqualsAndHashCode
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProductDto {
+
+    private Long id;
+    private String name;
+    private Integer price;
+    private Integer stock;
+
+}
