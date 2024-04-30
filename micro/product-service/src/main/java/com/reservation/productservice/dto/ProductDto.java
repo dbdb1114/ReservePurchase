e411222eea84cd95a6e7872e502c98c291b5e397
yujeonghyeon <dbdb1114@naver.com>
2024-04-30 18:52:54 +0900

    ProductDto 불필요 어노테이션 삭제

--- micro/product-service/src/main/java/com/reservation/productservice/dto/ProductDto.java
+++ micro/product-service/src/main/java/com/reservation/productservice/dto/ProductDto.java
@@ -9,7 +9,6 @@ import lombok.ToString;
 
 @Getter
 @Builder
-@ToString
 @EqualsAndHashCode
 @AllArgsConstructor
 @NoArgsConstructor
--- micro/user-service/src/main/java/com/reservation/userservice/dto/ProductDto.java
+++ micro/user-service/src/main/java/com/reservation/userservice/dto/ProductDto.java
@@ -5,13 +5,10 @@ import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.ToString;
 
 @Getter
-@Builder
-@ToString
-@EqualsAndHashCode
-@AllArgsConstructor
 @NoArgsConstructor
 public class ProductDto {
 
