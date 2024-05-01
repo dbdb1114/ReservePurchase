5be93aa823c9894f5d3b7cc23ece555dbf6aee64
yujeonghyeon <dbdb1114@naver.com>
2024-05-01 14:18:34 +0900

    ModelMapper 버전변경 #12

---
+++ micro/order-service/src/main/java/com/reservation/orderservice/config/ModelMapperConfig.java
@@ -0,0 +1,16 @@
+package com.reservation.orderservice.config;
+
+import org.modelmapper.ModelMapper;
+import org.modelmapper.config.Configuration.AccessLevel;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ModelMapperConfig {
+    @Bean
+    public ModelMapper modelMapper(){
+        ModelMapper mapper = new ModelMapper();
+        mapper.getConfiguration().setFieldAccessLevel(AccessLevel.PRIVATE).setFieldMatchingEnabled(true);
+        return mapper;
+    }
+}
