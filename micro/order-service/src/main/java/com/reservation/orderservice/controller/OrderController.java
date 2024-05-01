d11c874702a1fb9ff40a874bfdfebc0088cff085
yujeonghyeon <dbdb1114@naver.com>
2024-05-01 13:05:27 +0900

    주문 컨트롤러 메서드 변경 #12

--- micro/order-service/src/main/java/com/reservation/orderservice/controller/OrderController.java
+++ micro/order-service/src/main/java/com/reservation/orderservice/controller/OrderController.java
@@ -18,6 +18,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
@@ -27,14 +28,8 @@ public class OrderController {
     ProductClient productClient;
     MemberClient memberClient;
     OrderService orderService;
-    /**
-     * 요구사항
-     * 1. MemberId로 Order객체를 생성한다.
-     * 2. 요청받은 주문의 상품에 대한 정보를 가져온다. ( 가격 및 차후 재고처리 )
-     * 3. 상품에 대한 정보를 기반으로 OrderDto를 만들어준다.
-     * 4. Order객체와 orderItems를 기반으로 주문을 생성 후 저장한다.
-     * */
-    @GetMapping("/order")
+
+    @PostMapping("/order")
     public ResponseEntity makeOrder(@RequestBody ArrayList<OrderItemDto> orderItemList, HttpServletRequest request) {
         String authorization = request.getHeader("Authorization");
         Long memberId = memberClient.memberId(authorization);
@@ -59,4 +54,5 @@ public class OrderController {
             return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(ResponseStatus.FA.responseVo);
         }
     }
+
 }
