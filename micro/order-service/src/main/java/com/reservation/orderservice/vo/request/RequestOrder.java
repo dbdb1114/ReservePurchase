2191a0a606cf1279f15ce370b61c4980631da488
yujeonghyeon <dbdb1114@naver.com>
2024-05-03 14:09:54 +0900

    요청 및 응답간 VO전달을 용이하게 하기 위하여 Http~ 인터페이스를 구현하는 방향을 채택하고, feignClient 사용시 Http~의 구현체는 전부 입력값으로 사용하도록 수정

--- micro/order-service/src/main/java/com/reservation/orderservice/vo/request/RequestOrder.java
+++ micro/order-service/src/main/java/com/reservation/orderservice/vo/request/RequestOrder.java
@@ -1,17 +1,17 @@
-package com.reservation.orderservice.vo.response;
+package com.reservation.orderservice.vo.request;
 
 import com.reservation.orderservice.entity.OrderStatus;
 import com.reservation.orderservice.vo.HttpOrder;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
-import lombok.Data;
+import lombok.Getter;
 
-@Data
-public class ResponseOrder implements HttpOrder {
+@Getter
+public class RequestOrder implements HttpOrder {
     private Long id;
     private Long memberId;
-    private List<ResponseOrderItem> items = new ArrayList<>();
+    private List<RequestOrderItem> items = new ArrayList<>();
     private OrderStatus status;
     private LocalDateTime orderDate;
 }
--- micro/order-service/src/main/java/com/reservation/orderservice/vo/request/RequestOrderItem.java
+++ micro/order-service/src/main/java/com/reservation/orderservice/vo/request/RequestOrderItem.java
@@ -1,13 +1,13 @@
 package com.reservation.orderservice.vo.request;
 
 import com.reservation.orderservice.entity.Order;
-import lombok.Data;
+import com.reservation.orderservice.vo.HttpOrderItem;
 import lombok.Getter;
 
 @Getter
-public class RequestOrderItem {
+public class RequestOrderItem implements HttpOrderItem {
     private Long id;
-    private Order order;
+//    private Order order;
     private Long productId;
     private Integer quantity;
     private Integer orderPrice;
